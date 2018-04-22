ca3e9e96549fc5374b62ff12dd503ce9339bb88a
Half Scheidl <haschdl@users.noreply.github.com>
2018-04-22 18:11:13 +0200

    added touchmoved to prevent scrolling

--- orbitalChallenge/sketch.js
+++ orbitalChallenge/sketch.js
@@ -529,6 +529,10 @@ function createSolveButton() {
     });
 }
 
+//prevents scrolling. Is it working?
+function touchmoved() {
+  return false;
+}
 function mouseWheel(event) {
     t0.z = constrain(t0.z - event.delta / 5, -1000, 1000);
     //return false to block page scrolling
